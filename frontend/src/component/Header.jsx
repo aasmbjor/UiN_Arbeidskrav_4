b1573c8bee641cee0d7dfe161db8585af711d9b5
hahv98 <henden.ahv@gmail.com>
2026-04-20 19:48:02 +0200

    pågebynt jobb med header
    Utfylling av header og CSS

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function Header(){
     return(
-        <header>
-            <h1>UiN26_17</h1>
+        <header className="header">
+            <section className="nav-wrapper">
+            <Link to="/" className="logo"><h1>UiN26_17</h1></Link>
+                <nav className="nav-links">
+                    <Link to="/" className="nav-link">Hjem</Link>
+                    <Link to="/om-oss" className="nav-link">Om oss</Link>
+                    <Link to="/arbeidskrav" className="nav-link">Arbeidskrav</Link>
+                </nav>
+            </section>
         </header>
     )
 }
