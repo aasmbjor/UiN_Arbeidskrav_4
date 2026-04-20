5256fc549899e3b70758e7892e673a3b1e1b3d05
chrisaharaldsen94 <chris.a.haraldsen@hiof.no>
2026-04-20 09:21:44 +0200

    oppdatert profil chris

---
+++ frontend/src/component/Chris.jsx
@@ -0,0 +1,19 @@
+export default function Chris(){
+    return(
+        <article classname="profilkort-chris">
+        <figure>
+            <img
+            src="https://picsum.photos/200"
+            alt="chris"
+            />
+        </figure>
+        <section>
+            <h3>Chris Haraldsen</h3>
+            <p><strong>Studie:</strong>Bachelor Informasjonssystemer</p>
+            <address>
+                E-post: <a href="mailto:chrisnh@hiof.no">chrisanh@hiof.no</a>
+            </address>
+        </section>
+        </article> 
+    )
+}
