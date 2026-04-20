f7d1f294beae1f6252a12c6426b47089e5478dd5
chrisaharaldsen94 <chris.a.haraldsen@hiof.no>
2026-04-20 10:51:10 +0200

    endring

--- frontend/src/component/Chris.jsx
+++ frontend/src/component/Chris.jsx
@@ -9,7 +9,7 @@ export default function Chris(){
         </figure>
         <section>
             <h3>Chris Haraldsen</h3>
-            <p><strong>Studie:</strong>Bachelor Informasjonssystemer</p>
+            <p><strong>Studie:</strong>Bachelor i Informasjonssystemer</p>
             <address>
                 E-post: <a href="mailto:chrisnh@hiof.no">chrisanh@hiof.no</a>
             </address>
