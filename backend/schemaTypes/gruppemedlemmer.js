55cbd6e67e741fe1305593ed28800770e532c257
aasmbjor <aasmund.bjornerud@gmail.com>
2026-04-21 09:23:08 +0200

    Lagt til slug til gruppemedlemmer

--- backend/schemaTypes/gruppemedlemmer.js
+++ backend/schemaTypes/gruppemedlemmer.js
@@ -9,6 +9,18 @@ const gruppemedlemmer = {
             type: "string"
         },
         {
+            title: 'Slug',
+            name: 'slug',
+            type: 'slug',
+            options: {
+                source: 'navn',
+                slugify: input => input
+                                    .toLowerCase()
+                                    .replace(/\s+/g, '-')
+                                    .slice(0, 100)
+            }
+        },
+        {
             name: "epost",
             title: "E-Post",
             type: "string"
