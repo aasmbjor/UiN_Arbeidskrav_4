7d23302fdcdc4254f0102bb77a96eb2da6bc7d7e
obovolden <obovolden@gmail.com>
2026-04-21 10:25:18 +0200

    la til slug i link

--- frontend/src/component/Gruppemedlemmer.jsx
+++ frontend/src/component/Gruppemedlemmer.jsx
@@ -8,7 +8,7 @@ export default function Gruppemedlemmer(){
     useEffect(() => {
       /* Henter alle gruppemedlemmer fra Sanity databasen */
       async function fetchAllMedlemmer() {
-        const allMedlemmer = await client.fetch("*[_type == 'gruppemedlemmer']{_id, navn, epost, studie, bilde, 'imageURL': bilde.asset->url}")
+        const allMedlemmer = await client.fetch("*[_type == 'gruppemedlemmer']{_id, navn, slug, epost, studie, bilde, 'imageURL': bilde.asset->url}")
         setSanityMedlem(allMedlemmer)
       }
 
@@ -20,7 +20,7 @@ export default function Gruppemedlemmer(){
     return (
     <section id="member-list">
       <h2 className="undertitle">Gruppemedlemmer</h2> {/* Lagt til overskrift for listing av gruppemedlemmer */}
-      {sanityMedlem?.map((m) => (<Link key={m._id} to={"/"}> <Medlemskort key={m._id} m={m}/></Link>))}
+      {sanityMedlem?.map((m) => (<Link key={m._id} to={"/medlem/" + m.slug.current}> <Medlemskort key={m._id} m={m}/></Link>))}
     </section>)
 
 
