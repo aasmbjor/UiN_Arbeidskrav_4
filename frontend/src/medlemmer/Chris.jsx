ccf7fd978e6556e484fce67b6750c2d4e0820665
chrisaharaldsen94 <chris.a.haraldsen@hiof.no>
2026-04-21 09:43:44 +0200

    Tror jeg fikset useEffect med min groq query

--- frontend/src/medlemmer/Chris.jsx
+++ frontend/src/medlemmer/Chris.jsx
@@ -1,19 +1,40 @@
-export default function Chris(){
-    return(
-        <article classname="profilkort-chris">
-        <figure>
-            <img
-            src="https://picsum.photos/200"
-            alt="chris"
-            />
-        </figure>
-        <section>
-            <h3>Chris Haraldsen</h3>
-            <p><strong>Studie:</strong>Bachelor i Informasjonssystemer</p>
-            <address>
-                E-post: <a href="mailto:chrisnh@hiof.no">chrisanh@hiof.no</a>
-            </address>
-        </section>
+import { useEffect, useState } from 'react'
+import client from '../helpers/client'
+
+export default function Chris() {
+    const [minProfil, setMinProfil] = useState(null)
+
+    useEffect(() => {
+        async function fetchChris() {
+            // Henter fra Sanity
+            const data = await client.fetch(
+                `*[_type == "gruppemedlemmer" && navn == "Chris Haraldsen"][0]{
+                    "imageURL": bilde.asset->url
+                }`
+            )
+            setMinProfil(data)
+        }
+ 
+        fetchChris()
+    }, [])
+
+    return (
+        <article className="profilkort-chris">
+            <figure>
+                {minProfil && (
+                    <img
+                        src={minProfil.imageURL}
+                        alt="Chris"
+                    />
+                )}
+            </figure>
+            <section>
+                <h3>Chris Haraldsen</h3>
+                <p><strong>Studie: </strong>Bachelor i Informasjonssystemer</p>
+                <address>
+                    E-post: <a href="mailto:chrisanh@hiof.no">chrisanh@hiof.no</a>
+                </address>
+            </section>
         </article> 
     )
 }
