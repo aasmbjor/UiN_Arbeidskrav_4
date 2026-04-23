9005a237da7c9ff3e68e0964eb05ad700624fc8c
aasmbjor <aasmund.bjornerud@gmail.com>
2026-04-23 10:09:39 +0200

    Lagt til CSS til OmOss

--- frontend/src/Pages/OmOss.jsx
+++ frontend/src/Pages/OmOss.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import client from "../helpers/client"
+import '../style/omoss.css'
 
 export default function OmOss(){
   const [sanityAbout, setSanityAbout] = useState(null)
