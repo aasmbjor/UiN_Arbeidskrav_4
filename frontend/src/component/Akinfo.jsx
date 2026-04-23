6c00d062245653c4734c44810a1ec8fcc7602320
obovolden <obovolden@gmail.com>
2026-04-23 10:17:38 +0200

    Flyttet css til ny fil

--- frontend/src/component/Akinfo.jsx
+++ frontend/src/component/Akinfo.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import client from "../helpers/client"
+import '../style/omArbeidskrav.css'
 
 export default function Akinfo(){
     const parameters = useParams()
