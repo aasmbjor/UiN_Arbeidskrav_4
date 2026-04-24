57f0e2fd8c737f5e0c0fbac56128f6c2a81d51be
hahv98 <henden.ahv@gmail.com>
2026-04-24 13:04:35 +0200

    fjernet unik artikkel CSS

--- frontend/src/component/Medlemskort.jsx
+++ frontend/src/component/Medlemskort.jsx
@@ -1,6 +1,6 @@
 export default function Medlemskort({m}){
     return(
-        <article className={`member-card profil-${m.slug.current}`}>
+        <article className="member-card">
             <img className="imgsrc" src={m.imageURL} alt={m.navn} />
             <h3>{m.navn}</h3>
         </article>
